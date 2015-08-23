f1d8e4ce085b85fb33c879fa71953cba42bb82d8
Nash Kaminski <nashkaminski@gmail.com>
2015-08-23 02:03:49 -0500

    Initial Release

---
+++ IITLookup.py
@@ -0,0 +1,48 @@
+from pysimplesoap.client import SoapClient,SoapFault
+class IITLookup:
+
+        def __init__(self, wsurl):
+                self.sclient=SoapClient(wsdl=wsurl)
+
+        def nameByID(self,idnumber):
+                try:
+                    return self.sclient.PCSGetName(idNumber=idnumber)['PCSGetNameResult']
+                except SoapFault:
+                    return None
+
+        def nameIDByCard(self,cardnum):
+                try:
+                    ret = self.sclient.PCSGetbyCardNum(cardNumber=cardnum)['PCSGetbyCardNumResult']
+                except SoapFault:
+                    return None
+                if(ret == 'Not Found'):
+                        return None
+                ret = ret.split(',')
+                output = {}
+                output['last_name'] = ret[0]
+                output['first_name'] = ret[1]
+                output['middle_name'] = ret[2]
+                output['idnumber'] = ret[3]
+                return output
+
+        def inquiryByID(self,idnumber):
+                try:
+                    ret = self.sclient.PCSGetInquiry(idNumber=idnumber)['PCSGetInquiryResult']
+                except SoapFault:
+                    return None
+                output = {}
+                for x in ret:
+                    x = x['InquiryRecord']
+                    n_obj = {}
+                    n_obj['blocked'] = x['blocked']
+                    n_obj['limit'] = x['limit']
+                    n_obj['balance'] = x['balance']
+                    output[x['tender']] = n_obj
+                return output
+
+        def inquiryByCard(self,cardnum):
+                idn = self.nameIDByCard(cardnum)
+                if not idn:
+                        return None
+                return self.inquiryByID(idn['idnumber'])
+        
